f2ddb5a629420269c414b8aa967c18baf1917801
Sabrin <sabrin.byaah@protonmail.com>
2025-03-28 15:53:33 +0100

    update

--- javaScript/.idea/Exercises/Programmierer/index.js
+++ javaScript/.idea/Exercises/Programmierer/index.js
@@ -1,18 +1,17 @@
 import { Programmierer } from './Programmierer.js';
 
-let programmierer1 = new Programmierer("Cody","JavaScript");
+let programmierer1 = new Programmierer('Cody', 'JavaScript');
 console.log(programmierer1.gruss());
 console.log(programmierer1.laune());
 
-let programmierer2 = new Programmierer("Claude",["COBAL","Clojure"]);
+let programmierer2 = new Programmierer('Claude', ['COBAL', 'Clojure']);
 console.log(programmierer2.gruss());
 console.log(programmierer2.laune());
 
-
-programmierer1.auftrag("Website Optimierung","JavaScript",(erfolgreich) => {
+programmierer1.auftrag('Website Optimierung', 'JavaScript', (erfolgreich) => {
     if (erfolgreich) {
-        console.log("Sie haben den Auftrag erfolgreich abgeschlossen.");
+        console.log('Sie haben den Auftrag erfolgreich abgeschlossen.');
     } else {
-        console.log("Sie haben den Auftrag nicht abgeschlossen.");
+        console.log('Sie haben den Auftrag nicht abgeschlossen.');
     }
 });
